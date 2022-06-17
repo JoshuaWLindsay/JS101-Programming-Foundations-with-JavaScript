fbf4b91e5414cb82a56d28cfe61cef7768868160
Joshua Lindsay <99047296+JoshuaWLindsay@users.noreply.github.com>
2022-06-17 06:55:53 -0500

    update to attempt creating branch

---
+++ Lesson-2-Small-Programs/pseudocode.js
@@ -0,0 +1,116 @@
+/*
+START Write a function that 'returns' the 'sum' of 'two' numbers.
+
+SET Let Function include two numbers as two parameters.
+
+GET Two parameters, one for each number.
+
+PRINT Return parameter1 + parameter2
+
+READ
+
+IF/ELSE IF/ELSE
+
+WHILE
+
+END
+
+*/
+
+/*
+
+START Write a function that takes an array of strings,
+and returns a string that is all those strings concatenated together.
+
+SET Let Function include one parameter for array of strings.
+
+SET variable 'arrayConcat' equal to concatenating elements in array to combine, separated by ''.
+
+PRINT Return variable 'arrayConcat'
+
+READ
+
+IF/ELSE IF/ELSE
+
+WHILE
+
+END
+
+*/
+
+/*
+
+START a method that takes an array of integers,
+and returns a new array with every other element from the original array,
+starting with the first element.
+
+SET function with one parameter of an array of integers.
+
+Let variable evenArray equal to [].
+
+FOR every element in array starting at 0, counting by 2:
+  push element to new 'evenArray'.
+
+PRINT
+
+Return 'evenArray'
+
+READ
+
+IF/ELSE IF/ELSE
+
+WHILE
+
+END
+
+*/
+
+/*
+
+START a function that determines the index of the 3rd occurrence of a given character in a string.
+For instance, if the given character is 'x' and the string is 'axbxcdxex',
+the function should return 6 (the index of the 3rd 'x').
+If the given character does not occur at least 3 times, return null.
+
+SET
+
+Let Function include two numbers as two parameters.
+
+GET
+
+Two parameters, one for each number.
+
+PRINT
+
+Return parameter1 + parameter2
+
+READ
+
+IF/ELSE IF/ELSE
+
+WHILE
+
+END
+
+*/
+
+/*
+START a function that takes two arrays of numbers and returns the result of merging the arrays.
+The elements of the first array should become the elements at the even indexes of the returned array,
+while the elements of the second array should become the elements at the odd indexes.
+
+SET Let Function include two numbers as two parameters.
+
+GET Two parameters, one for each number.
+
+PRINT Return parameter1 + parameter2
+
+READ
+
+IF/ELSE IF/ELSE
+
+WHILE
+
+END
+
+*/
